1196c131ccf5eb33134f6999ffe75cf01598d6e1
Pskalou <pascal.padilla@gmail.com>
2022-09-21 17:03:38 +0200

    récupération des dimensions à partir d'un texte

--- main.py
+++ main.py
@@ -11,10 +11,19 @@ class Life():
     def __init__(self):
         # grille actuelle
         self.grid = ""
+        self.height = 0
+        self.width = 0
 
 
-    def set_grid(self, text_grid: str) -> None:
+    def text_to_grid(self, text_grid: str) -> None:
         # setter pour la grille actuelle
+        
+        # transformation de la grille en tableau de string
+        tab_grid =  text_grid.split("\n")
+        # récupération de la hauteur et la largeur de la grille
+        self.height, self.width = map(int, tab_grid[0].split() )
+        # print(f"INFO: height = {h-1+1}, width = {w-1+1}")
+
         self.grid = text_grid
 
 
