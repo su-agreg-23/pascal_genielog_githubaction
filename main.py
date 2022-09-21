821b45bf3f732c2c358952e058be0f8c0f7a9629
Pskalou <pascal.padilla@gmail.com>
2022-09-21 16:44:26 +0200

    mise en place de la classe

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ développent :
     * CI (via github)
 """
 
+class Life():
+    # classe de jeu de la vie
+
+    def __init__(self):
+        # grille actuelle
+        self.grid = ""
+
+
+    def set_grid(self, text_grid: str) -> None:
+        # setter pour la grille actuelle
+        self.grid = text_grid
+
+
+    def next(self):
+        # méthode qui renvoie la prochaine grille en fonction
+        # de self.grid actuel
+        # respecte les règles du jeu de la vie
+        return self.grid
+
--- main_test.py
+++ main_test.py
@@ -10,10 +10,29 @@ def test_integration_ok():
     assert not (1 > 2)
 
 
-# def test_class_creation():
-#     # création d'une classe Life du jeu de la vie
-#     try:
-#         g = Life()
-#     except NameError:
-#         raise (NameError("la classe n'existe pas"))
+def test_class_creation():
+    # création d'une classe Life du jeu de la vie
+    try:
+        g = Life()
+    except NameError:
+        raise (NameError("la classe n'existe pas"))
+
+
+def test_next_method():
+    # vérifie l'existence de
+    #   * attribut grid
+    #   * méthode next
+    
+    # grille vide format texte
+    grid = """
+4 8
+........
+........
+........
+........
+"""
+    # créer une grille vide
+    g = Life()
+    g.set_grid(grid)
+    assert g.next() == grid
 
